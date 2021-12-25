1846504f510d54037b35993e31083a30fea43445
poo <pbrenna12@gmail.com>
2021-12-25 17:24:43 +0800

    V2.6 implement "Hello World" port. address: http://127.0.0.1:8081/hello

--- src/main/java/com/rck/wiki/config/WikiApplication.java
+++ src/main/java/com/rck/wiki/config/WikiApplication.java
@@ -1,8 +1,10 @@
-package com.rck.wiki;
+package com.rck.wiki.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.rck.wiki")
 @SpringBootApplication
 public class WikiApplication {
 
